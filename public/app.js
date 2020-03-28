297b7c3312e1a324b40755f0ac690e3d0577f3ab
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 20:38:00 +0530

    image file upload on firebase server

--- public/app.js
+++ public/app.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
-  const db = firebase.firestore();
-  const productsRef = db.collection('products');
-
-  const query = productsRef.orderBy('price').limit(2);
-
-  query.get().then(products => {
-    products.forEach(doc => {
-      data = doc.data();
-      document.write(`${data.name} at $${data.price} <br>`);
-    });
-  });
 });
+
+function uploadFile(files) {
+  const storageRef = firebase.storage().ref();
+  const horseRef = storageRef.child('horse.jpg');
+
+  const file = files.item(0);
+
+  const task = horseRef.put(file);
+
+  task.then(snpashot => {
+    console.log(snpashot);
+    const url = snpashot.downloadURL;
+    document.querySelector('#imgUpload').setAttribute('src', url);
+  });
+}
