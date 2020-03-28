f306923de845773f83e49e35bca79ad68aac93ff
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 17:47:26 +0530

    document fetch from firebase server

--- public/app.js
+++ public/app.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
-});
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('mypost');
 
-function googleLogin() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(result => {
-      const user = result.user;
-      document.write(`Hello ${user.displayName}`);
-      console.log(user);
-    })
-    .catch(console.log);
-}
+  myPost.onSnapshot(doc => {
+    const data = doc.data();
+    document.write(data.title + `<br>`);
+    document.write(data.createdAt);
+  });
+});
