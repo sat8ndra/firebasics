b07dae8c3e7805908d5b547dc3ea49a6c5380e16
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 19:28:19 +0530

    realtime change in firebase database by input field

--- public/app.js
+++ public/app.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', event => {
 
   myPost.onSnapshot(doc => {
     const data = doc.data();
-    document.write(data.title + `<br>`);
-    document.write(data.createdAt);
+    document.querySelector('#title').innerHTML = data.title;
   });
 });
+
+function updatePost(e) {
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('firstpost');
+  myPost.update({ title: e.target.value });
+}
