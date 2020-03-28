b38ead0d9962c5416ced2ea808ad8182b9254642
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 18:45:51 +0530

    document fetch from firebase server

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', event => {
   const app = firebase.app();
   const db = firebase.firestore();
-  const myPost = db.collection('posts').doc('mypost');
+  const myPost = db.collection('posts').doc('firstpost');
 
-  myPost.onSnapshot(doc => {
+  myPost.get().then(doc => {
     const data = doc.data();
     document.write(data.title + `<br>`);
     document.write(data.createdAt);
