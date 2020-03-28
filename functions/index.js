040d8a76cf99a76948ee9a27ecdf7460f41aa9a8
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 21:38:38 +0530

    firebase cloud function working on sendMessage function every time any product created in firestore it adds a message string

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendMessage = functions.firestore
+  .document('products/{productId}')
+  .onCreate(event => {
+    const docId = event.params.productId;
+    const name = event.data.data().name;
+    const productRef = admin
+      .firestore()
+      .collection('products')
+      .doc(docId);
+    return productRef.update({
+      message: `Nice ${name}! - Love Cloud Functions`
+    });
+  });
