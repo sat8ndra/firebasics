2332ade1553d6d78f306469f8574d1ee0648985e
satyendra gupta <sat8ndra@gmail.com>
2020-03-28 20:01:37 +0530

    productlist fetch by orderBy and limit

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', event => {
   const db = firebase.firestore();
   const productsRef = db.collection('products');
 
-  const query = productsRef.orderBy('price', 'desc');
+  const query = productsRef.orderBy('price').limit(2);
 
   query.get().then(products => {
     products.forEach(doc => {
